4d3321c7ebe690c9711f08553a535a93c759e3c8
Lipe-to <fbboregio2020@gmail.com>
2025-09-19 13:31:59 -0300

    ajustando diretorios

--- src/main/java/com/codcoz/servlet/endereco/ServletUpdateEndereco.java
+++ src/main/java/com/codcoz/servlet/endereco/ServletUpdateEndereco.java
@@ -1,4 +1,4 @@
-package com.codcoz.servlet;
+package com.codcoz.servlet.endereco;
 
 import com.codcoz.dao.EnderecoDAO;
 import com.codcoz.model.Endereco;
@@ -9,8 +9,8 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "ServletDeleteEndereco", value = "/ServletDeleteEndereco")
-public class ServletDeleteEndereco extends HttpServlet {
+@WebServlet(name = "ServletUpdateEndereco", value = "/ServletUpdateEndereco")
+public class ServletUpdateEndereco extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -18,9 +18,21 @@ public class ServletDeleteEndereco extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
+//          cria o objeto Endereco já com os dados do form
+        Endereco endereco = new Endereco(
+            Integer.parseInt(request.getParameter("id")),
+            request.getParameter("rua"),
+            request.getParameter("complemento"),
+            request.getParameter("cidade"),
+            request.getParameter("estado"),
+            request.getParameter("cep"),
+            request.getParameter("numero")
+        );
+
+//          chama o DAO para update
         EnderecoDAO enderecoDAO = new EnderecoDAO();
-        enderecoDAO.delete(id);
+        enderecoDAO.update(endereco);
+//          redireciona pra o read
         List<Endereco> lista = enderecoDAO.read();
 
         // Define a lista como atributo da request
